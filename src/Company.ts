ed9691ecfa5ff243b43630dcd3df6441415a8249
Juhye O <artigen3@gmail.com>
2024-05-06 22:37:49 +0900

    Adding Google Maps support

--- src/Company.ts
+++ src/Company.ts
@@ -1,1 +1,19 @@
-class Company {}
+import { faker } from '@faker-js/faker';
+
+export class Company {
+  companyName: string;
+  catchPrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.companyName = faker.company.name();
+    this.catchPrase = faker.company.catchPhrase();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+  }
+}
