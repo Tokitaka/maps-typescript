0b2c855ba96854bb5b3119229484158440c3cbd5
Juhye O <artigen3@gmail.com>
2024-05-07 20:47:06 +0900

    DUPLICATE CODE : addMarkeron User / Company

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -27,5 +27,13 @@ export class CustomMap {
       },
     });
   }
-  addMarkerOnCompany(company: Company): void {}
+  addMarkerOnCompany(company: Company): void {
+    new google.maps.marker.AdvancedMarkerElement({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
+  }
 }
