81447184de4c4c2ad84a00dfc88dcac4b50737a9
Juhye O <artigen3@gmail.com>
2024-05-07 20:44:03 +0900

    migrate Marker to AdvancedMarkerElement

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,10 +1,14 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor() {
+  constructor(divId: string) {
     this.googleMap = new google.maps.Map(
-      document.getElementById('map') as HTMLElement,
+      document.getElementById(divId) as HTMLElement,
       {
+        mapId: 'DEMO_MAP_ID',
         center: {
           lat: 0,
           lng: 0,
@@ -13,4 +17,15 @@ export class CustomMap {
       }
     );
   }
+
+  addMarkerOnUser(user: User): void {
+    new google.maps.marker.AdvancedMarkerElement({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+  addMarkerOnCompany(company: Company): void {}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-// import { User } from './User';
+import { User } from './User';
 // import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
 //Show the location of user and company as markers on Google Map
+const user = new User();
+const customMap = new CustomMap('map');
+
+customMap.addMarkerOnUser(user);
